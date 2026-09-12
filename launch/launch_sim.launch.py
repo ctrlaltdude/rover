113fd5f833d677282b4c993ba9323fef389b69a0
ctrlaltdude <hans_debruin@hotmail.com>
2026-09-12 16:57:33 +0200

    tweaking

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -49,8 +49,8 @@ def generate_launch_description():
 
     world_arg = DeclareLaunchArgument(
         'world',
-        #default_value="src/rover/worlds/hanstestground.sdf",
-        default_value="empty.sdf",
+        default_value=os.path.join(
+            get_package_share_directory(package_name), 'worlds', 'empty.world'),
         description='World to load'
         )
 
